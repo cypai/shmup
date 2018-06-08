ec6d8875037505f8219cb6ffd83af082febe349c
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-06-07 21:15:16 -0700

    Draw score and lives

--- core/src/main/java/com/pipai/shmup/artemis/systems/RenderingSystem.java
+++ core/src/main/java/com/pipai/shmup/artemis/systems/RenderingSystem.java
@@ -5,6 +5,7 @@ import com.artemis.BaseSystem;
 import com.artemis.ComponentMapper;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.pipai.shmup.ShmupGame;
@@ -26,11 +27,13 @@ public class RenderingSystem extends BaseSystem {
 
     private SpriteBatch spr;
     private ShapeRenderer shapeRenderer;
+    private BitmapFont font;
 
     public RenderingSystem(ShmupGame game) {
         config = game.getConfiguration();
         spr = game.getSpriteBatch();
         shapeRenderer = game.getShapeRenderer();
+        font = game.getFont();
     }
 
     @Override
@@ -53,9 +56,20 @@ public class RenderingSystem extends BaseSystem {
     }
 
     private void renderUi() {
+        float uiLeft = config.getRightBound();
+        float uiWidth = config.getUiWidth();
+        float uiHeight = Gdx.graphics.getHeight();
+        float padding = 36f;
+
         shapeRenderer.setColor(Color.GRAY);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.rect(config.getRightBound(), 0, config.getUiWidth(), Gdx.graphics.getHeight());
+        shapeRenderer.rect(uiLeft, 0, uiWidth, uiHeight);
         shapeRenderer.end();
+
+        float lineHeight = font.getLineHeight();
+        spr.begin();
+        font.draw(spr, "Score: " + sGameState.score, uiLeft + padding, uiHeight - padding);
+        font.draw(spr, "Lives: " + sGameState.lives, uiLeft + padding, uiHeight - padding - lineHeight);
+        spr.end();
     }
 }
