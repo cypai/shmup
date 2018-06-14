172f76bec9c6653bc39715ae8967439dc695de96
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-06-14 04:11:02 -0700

    sample impl: enemy fires more bullets

--- core/src/main/java/com/pipai/shmup/artemis/systems/EnemySimpleAiSystem.java
+++ core/src/main/java/com/pipai/shmup/artemis/systems/EnemySimpleAiSystem.java
@@ -6,6 +6,7 @@ import com.artemis.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.pipai.shmup.ShmupGame;
+import com.pipai.shmup.Utils;
 import com.pipai.shmup.artemis.Configuration;
 import com.pipai.shmup.artemis.components.*;
 
@@ -48,7 +49,9 @@ public class EnemySimpleAiSystem extends IteratingSystem {
         cAi.bulletTimer -= 1;
         if (cAi.bulletTimer <= 0) {
             cAi.bulletTimer = cAi.bulletDelay;
-            createBullet(cXy.x, cXy.y, (float) Math.PI * 3 / 2, 10f);
+            for (int i = 0; i < 20; i += 1) {
+                createBullet(cXy.x, cXy.y, Utils.RNG.nextFloat() * 2 * (float) Math.PI, 5f);
+            }
         }
     }
 
