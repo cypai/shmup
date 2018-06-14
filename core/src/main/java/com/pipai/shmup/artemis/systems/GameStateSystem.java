5b963b21c01a98bdfef930e9c44d7cd4f2056f09
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-06-14 04:06:09 -0700

    Exit game when lives run out

--- core/src/main/java/com/pipai/shmup/artemis/systems/GameStateSystem.java
+++ core/src/main/java/com/pipai/shmup/artemis/systems/GameStateSystem.java
@@ -1,6 +1,7 @@
 package com.pipai.shmup.artemis.systems;
 
 import com.artemis.BaseSystem;
+import com.badlogic.gdx.Gdx;
 
 public class GameStateSystem extends BaseSystem {
 
@@ -11,6 +12,7 @@ public class GameStateSystem extends BaseSystem {
     protected void processSystem() {
         if (lives == 0) {
             System.out.println("Oh no! Game over. Your final score was: " + score);
+            Gdx.app.exit();
         }
     }
 }
