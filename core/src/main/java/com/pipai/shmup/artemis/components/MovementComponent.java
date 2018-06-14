74d5a82ebd5c4a6f2f87dfaa1cd2d415be86d766
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-06-14 03:45:20 -0700

    Add setDirectionDegrees for those who don't know radians

--- core/src/main/java/com/pipai/shmup/artemis/components/MovementComponent.java
+++ core/src/main/java/com/pipai/shmup/artemis/components/MovementComponent.java
@@ -5,4 +5,8 @@ import com.artemis.Component;
 public class MovementComponent extends Component {
     public float direction;
     public float speed;
+
+    public void setDirectionDegrees(float degrees) {
+        direction = (float) (degrees / 180 * Math.PI);
+    }
 }
